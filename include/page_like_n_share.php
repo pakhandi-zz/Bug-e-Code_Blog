ff03374776d8d5059462ceb92a6931150efb0be0
pakhandi <asimkprasad@gmail.com>
2014-11-23 08:20:19 +0530

    including insights
    Connecting Social Networks

---
+++ include/page_like_n_share.php
@@ -0,0 +1,11 @@
+<div class="well">
+	<center><h4>Like & Share This blog on FB</h4></center>
+	<div class="fb-like" 
+		 data-href="https://www.facebook.com/pages/Bug-e-Code/758305594205082" 
+		 data-width="200" 
+		 data-layout="standard" 
+		 data-action="like" 
+		 data-show-faces="true" 
+		 data-share="true">
+	</div>
+</div>
